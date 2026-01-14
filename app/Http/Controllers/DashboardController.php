bfec9eda3167d52b35f051ed3142527ceed64e4a
andikesawa <andikesawa05@gmail.com>
2026-01-14 16:20:48 +0800

    buat halaman dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view("dashboard");
+        $data = array(
+            "title"=> "Dashboard",
+            "menuDashboard"=> "active",
+        );
+
+        return view("dashboard", $data);
     }
 }
