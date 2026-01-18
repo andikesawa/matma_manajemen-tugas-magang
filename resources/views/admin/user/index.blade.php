0f529b3db1616dfe86163741475f84a3366ca3d1
andikesawa <andikesawa05@gmail.com>
2026-01-19 04:10:25 +0800

    buat halaman user

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts/app')
+@section("content")
+<h1 class="h3 mb-4 text-gray-800"><i class="fas fa-user-alt mr-2"></i>{{ $title }}</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
 Route::get("dashboard", [DashboardController::class, "index"])->name('dashboard');
-
 Route::get("login", [AuthController::class,"login"])->name("login");
+Route::get("user", [UserController::class,"index"])->name("user");
